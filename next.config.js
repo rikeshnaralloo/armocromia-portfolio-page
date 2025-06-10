c7c446269bd0a4d6193f808f20df64f91cc71781
rikeshnaralloo <39901247+rikeshnaralloo@users.noreply.github.com>
2025-06-10 09:08:10 +0200

    Added integration with Contentful

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "images.ctfassets.net",
+        port: '',
+        pathname: '/{space_id}/**',
       },
     ],
   },
